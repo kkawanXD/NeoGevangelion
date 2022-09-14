2e567dd360652ded0f2bcca997e556bc7fcde457
kakawan <106829686+kkawanXD@users.noreply.github.com>
2022-09-14 20:31:19 -0300

    Delete conexao.php

--- conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$host="localhost";
-	$username="root";
-	$password="usbw";
-	$db_name="cadastro";
-	
-	$conect = @mysqli_connect($host,$username,$password,$db_name) 
-	or die ("Problemas com a conexao do Banco de Dados");
-?>
